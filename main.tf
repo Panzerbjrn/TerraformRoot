d1d92962f3442f0690d8e02e9ea4d49b650d9da0
Lars.Panzerbjrn <lars@panzerbjrn.eu>
2024-07-03 17:14:52 +0200

    Refreshing

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "azurerm_cognitive_account" "main" {
 
   location            = azurerm_resource_group.rg-1.location
   resource_group_name = azurerm_resource_group.rg-1.name
-  resource_group      = azurerm_resource_group.rg-1
+#  resource_group      = azurerm_resource_group.rg-1
   kind                = "OpenAI"
   sku_name            = "S0"
 }
