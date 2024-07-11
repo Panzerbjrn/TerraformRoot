6968c5e03e4a2c698856795ecc16824006d09ae0
Lars.Panzerbjrn <lars@panzerbjrn.eu>
2024-07-11 10:20:41 +0200

    Refreshing

--- outputs.tf
+++ outputs.tf
@@ -18,3 +18,7 @@ output "Module_AI_Services_Account" {
     sensitive = true
 }
 output "Module_AI_Services_Account_Id" { value = module.AI_Services_Account.id }
+
+output "current_client_id" { value = data.azurerm_client_config.current.client_id }
+output "current_subscription_id" { value = data.azurerm_client_config.current.subscription_id }
+output "current_tenant_id" { value = data.azurerm_client_config.current.tenant_id }
