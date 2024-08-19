d138f7b458d6165365e90148f9c32f7fb5f9c806
Lars.Panzerbjrn <lars@panzerbjrn.eu>
2024-08-19 17:21:13 +0200

    Refreshing

---
+++ imports.tf
@@ -0,0 +1,9 @@
+# import {
+#   to = module.ref_net.module.route_table["2"].azurerm_route_table.main
+#   id = "/subscriptions/55e28b37-2fff-4aba-94ed-6cb587bd1e85/resourceGroups/sdg-dev-uc1-devopstest-rg-01/providers/Microsoft.Network/routeTables/sdg-dev-uc1-devopstest-rt-internal-01"
+# }
+
+import {
+  to = module.ref_net.module.route_table["1"].azurerm_route_table.main
+  id = "/subscriptions/55e28b37-2fff-4aba-94ed-6cb587bd1e85/resourceGroups/sdg-dev-uc1-devopstest-rg-01/providers/Microsoft.Network/routeTables/sdg-dev-uc1-devopstest-rt-dmz-01"
+}
